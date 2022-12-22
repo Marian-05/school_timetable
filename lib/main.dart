174aee450ebba3ac52c6759393a6633d8ab5002e
Marian-05 <arfiremarian@gmail.com>
2022-12-22 11:36:02 +0200

    am inceput sa fc aplicatia

--- lib/main.dart
+++ lib/main.dart
@@ -3,16 +3,19 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(const MyApp());
 }
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child:const home+
+    return MaterialApp(
+      home: const App(),
+      theme: ThemeData(primarySwatch: Colors.blue),
     );
   }
 }
+
 class App extends StatefulWidget {
   const App({super.key});
 
