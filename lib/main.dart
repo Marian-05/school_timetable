77a4945e3a31424e381061a9c843b6b2e4a80d37
Marian-05 <arfiremarian@gmail.com>
2022-12-22 11:41:40 +0200

    dada

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: const App(),
       theme: ThemeData(primarySwatch: Colors.blue),
     );
