2f4b97ae58a4194a9d53068fc0d660135693eb66
DaRobij <robert.valentino.ciobanu@gmail.com>
2023-01-04 21:57:48 +0200

    gata

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,14 @@
-import 'package:flutter/material.dart';
+// ignore_for_file: prefer_const_constructors
 
-void main() {
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
@@ -12,7 +20,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: const App(),
-      theme: ThemeData(primarySwatch: Color.fromARGB(255, 177, 4, 177)),
+      theme: ThemeData(primarySwatch: Colors.blue),
     );
   }
 }
