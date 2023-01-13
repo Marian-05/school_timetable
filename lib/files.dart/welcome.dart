328465736641cbc218798c07741149b5c35815c1
Marian-05 <arfiremarian@gmail.com>
2023-01-13 18:09:55 +0200

    am incercat

--- lib/files.dart/welcome.dart
+++ lib/files.dart/welcome.dart
@@ -8,10 +8,24 @@ class WelcomePage extends StatefulWidget {
 }
 
 class WelcomePageState extends State<WelcomePage> {
+  List images = [
+    "poza1.jpg"
+    //  "poza2.jpg"
+    // "poza3.jpg"
+  ];
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text("BUNA")),
-    );
+    return Scaffold(
+        body: PageView.builder(
+            scrollDirection: Axis.vertical,
+            itemCount: images.length,
+            itemBuilder: (_, index) {
+              return Container(
+                  width: double.maxFinite,
+                  height: double.maxFinite,
+                  decoration: BoxDecoration(
+                      image: DecorationImage(
+                          image: AssetImage("images/${images[index]}"))));
+            }));
   }
 }
