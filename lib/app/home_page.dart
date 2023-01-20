d0fe28cb0b0b1b1aaff130fec33b9adf730546b5
Marian-05 <arfiremarian@gmail.com>
2023-01-20 13:26:28 +0200

    home page beginning

--- lib/app/home_page.dart
+++ lib/app/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:school_timetable/app/profile.dart';
 import 'package:school_timetable/app/search.dart';
 
@@ -12,8 +13,42 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: const Center(child: Text("Home Page")),
-    );
+    return Scaffold(
+        body: Column(
+      children: [
+        Container(
+          child: Padding(
+            padding: const EdgeInsets.fromLTRB(20, 50, 0, 0),
+            child: Row(children: [
+              Icon(Icons.menu, size: 30, color: Colors.black),
+              Expanded(child: Container()),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                    width: 50,
+                    height: 50,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        color: Colors.grey.withOpacity(0.5))),
+              ),
+            ]),
+          ),
+        ),
+        const SizedBox(height: 12),
+        Container(
+            child: Padding(
+          padding: const EdgeInsets.fromLTRB(30, 0, 20, 50),
+          child: Row(children: [
+            Text(
+              "Titlu",
+              style: GoogleFonts.abrilFatface(
+                  fontSize: 40,
+                  color: Colors.blue,
+                  fontStyle: FontStyle.italic),
+            )
+          ]),
+        )),
+      ],
+    ));
   }
 }
--- lib/app/main_page.dart
+++ lib/app/main_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:school_timetable/app/profile.dart';
 import 'package:school_timetable/app/search.dart';
-
+import 'package:google_fonts/google_fonts.dart';
 import 'home_page.dart';
 
 class MainPage extends StatefulWidget {
@@ -25,18 +25,21 @@ class _MainPageState extends State<MainPage> {
     return Scaffold(
         body: pages[curent_index],
         bottomNavigationBar: BottomNavigationBar(
-            // type: BottomNavigationBarType.shifting,
+            type: BottomNavigationBarType.shifting,
+            selectedItemColor: Colors.black,
+            unselectedItemColor: Colors.grey.withOpacity(0.5),
             onTap: OnTap,
             currentIndex: curent_index,
-            // showSelectedLabels: false,
-            // showUnselectedLabels: false,
+            unselectedFontSize: 0,
+            selectedFontSize: 0,
+            showSelectedLabels: false,
+            showUnselectedLabels: false,
             items: const [
+              BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
               BottomNavigationBarItem(
-                  icon: Icon(Icons.abc_sharp), label: "Home"),
+                  icon: Icon(Icons.search), label: "Search"),
               BottomNavigationBarItem(
-                  icon: Icon(Icons.abc_sharp), label: "Search"),
-              BottomNavigationBarItem(
-                  icon: Icon(Icons.abc_sharp), label: "Profile"),
+                  icon: Icon(Icons.person), label: "Profile"),
             ]));
   }
 }
